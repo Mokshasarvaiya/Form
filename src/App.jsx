29c1ae030c7bb4f8cb03e1f3988f116f5caa3efd
Mokshasarvaiya <safar@0101>
2023-09-20 20:22:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Form_val from './components/Form_val'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <div className="app">
+      <div className="container">
+        <div className="card">
+        <Form_val /> 
+        </div>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Form_val.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+
+
+const Form_val = () => {
+
+  
+  return (
+    <div>
+      <form className='form'>
+      <h1>Register Form</h1>
+      <hr />
+      <label htmlFor="">Name:</label>
+      <input type="text" placeholder='Enter your Name' className='input' />
+      <label htmlFor="">Address:</label>
+      <input type="text" className='input'  name="" id="" />
+      <label htmlFor="">Email:</label>
+      <input type="email" className='input' name="" id="" />
+      <label htmlFor="">Mobile.No:</label>
+      <input type="number" name="" id="" className='input' /><br />
+      <label htmlFor="">Gender:</label> &nbsp;&nbsp;
+      <input type="radio" name="gender" id="male" />Male &nbsp;&nbsp;
+      <input type="radio" name="gender" id="female"/>Female
+      <br />
+      <label htmlFor="">City:</label>
+      <select name="" id="" className='input'>
+      <option name="select">Select</option>
+      <option name="Ahmedabad">Ahmedabad</option>
+      <option name="Surat">Surat</option>
+      <option name="Baroda">Baroda</option>
+      <option name="Gandhinagar">Gandhinagar</option>
+      <option name="Bhavnagar">Bhavnagar</option>
+      <option name="Junagadh">Junagadh</option>
+      </select>
+     <h3>
+        <div ><input type="checkbox" name="" id="" className='checkbox' />&nbsp;
+      I Agree  <span> Company Terms</span> And <span> Policy</span></div></h3>
+      
+       <button className='button' >SIGN UP</button> &nbsp; &nbsp;
+       <button className='button2'>RESET</button>
+
+ </form>
+    </div>
+  )
+}
+
+export default Form_val
